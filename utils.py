7504f6f668c5fc68d4ee3cc3cc850d9d9060ddc4
Lethgir <lethgir@gmail.com>
2016-02-09 11:02:18 +0100

    add signature utils

---
+++ utils.py
@@ -0,0 +1,42 @@
+class utils:
+
+
+	def __init__(self):
+
+	# Initialiser aleatoirement les attributs de plusieurs individus
+	# contenus dans un tableau
+	def initialisation(self, individus):
+
+		return parents
+
+	# Evalue chaque individus contenus dans un tableau
+	def evaluation(self, individus):
+		eval=0
+
+		return eval;
+
+	#Selectionne deux parents a partir d'un tableau d'individus
+	# et renvoie un tableau avec les deux individus choisis
+	def selectParental(self, parents):
+
+
+		parentsFinaux = [parents[1], parents[2]]
+		return parentsFinaux
+
+
+	# Recois un tableau de deux individus et les croise
+	# pour retourner un enfant
+	def croisement(self, parents):
+		enfant= parents[1] + parents[2]
+
+		return enfant
+
+
+	# Modifie les attributs d'un individu en fonction d'un
+	# taux de mutation passe en parametre
+	def mutation(self, indiv, taux=5):
+
+		return indiv
+
+
+
